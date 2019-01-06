b5e7560752e9201281b340c29012535d3fa25973
LAPTOP-KP8I9NG8\Anissa Faik <anissa.faik@student.ucll.be>
2019-01-06 14:04:08 +0100

    final commit 3

--- src/main/java/be/ucll/da/dentravak/Application.java
+++ src/main/java/be/ucll/da/dentravak/Application.java
@@ -1,8 +1,6 @@
 package be.ucll.da.dentravak;
 
 
-import be.ucll.da.dentravak.model.Brood;
-import be.ucll.da.dentravak.model.Order;
 import be.ucll.da.dentravak.model.Sandwich;
 import be.ucll.da.dentravak.repository.OrderRepository;
 import be.ucll.da.dentravak.repository.SandwichRepository;
@@ -11,17 +9,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
-import static be.ucll.da.dentravak.model.Order.OrderBuilder.aOrder;
 import static be.ucll.da.dentravak.model.Sandwich.SandwichBuilder.aSandwich;
 
 @EnableDiscoveryClient
@@ -63,30 +59,7 @@ public class Application {
 
             repository.save(s1);
             repository.save(s2);
-
-            Order o = aOrder()
-                    .withMobilePhoneNumber("0480000000")
-                    .withCreationDate(LocalDateTime.of(2018, 3, 3, 3, 3, 3))
-                    .withBreadType(Brood.WRAP)
-                    .withSandwichId(s1.getId())
-                    .withPrice(new BigDecimal(2.00))
-                    .withName("testnaam")
-                    .withPrinted(false)
-                    .build();
-
-            Order o2 = aOrder()
-                    .withMobilePhoneNumber("0480000044")
-                    .withCreationDate(LocalDateTime.of(2018, 4, 4, 3, 3, 3))
-                    .withBreadType(Brood.WRAP)
-                    .withSandwichId(s2.getId())
-                    .withPrice(new BigDecimal(2.00))
-                    .withName("testnaam")
-                    .withPrinted(false)
-                    .build();
-
-
-            orderRepository.save(o);
-            orderRepository.save(o2);
+            
         };
     }
 }
